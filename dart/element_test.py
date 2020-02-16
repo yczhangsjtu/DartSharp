b8b10ae0cd300387018b8e814c68040ed9466a8e
Yuncong <yczhangsjtu@gmail.com>
2020-02-16 16:45:00 +0800

    Restructure.

---
+++ dart/element_test.py
@@ -0,0 +1,143 @@
+import unittest
+
+lazy_set_func="""
+NodeMetadata lazySet(
+  NodeMetadata meta, {
+  BuildOp buildOp,
+  Color color,
+  bool decoOver,
+  bool decoStrike,
+  bool decoUnder,
+  TextDecorationStyle decorationStyle,
+  CssBorderStyle decorationStyleFromCssBorderStyle,
+  String fontFamily,
+  String fontSize,
+  bool fontStyleItalic,
+  FontWeight fontWeight,
+  bool isBlockElement,
+  bool isNotRenderable,
+  Iterable<BuildOp> parentOps,
+  Iterable<String> styles,
+  Iterable<String> stylesPrepend,
+}) {
+  meta ??= NodeMetadata();
+
+  if (buildOp != null) {
+    meta._buildOps ??= [];
+    final ops = meta._buildOps as List<BuildOp>;
+    if (ops.indexOf(buildOp) == -1) {
+      ops.add(buildOp);
+    }
+  }
+
+  if (color != null) meta.color = color;
+
+  if (decoStrike != null) meta.decoStrike = decoStrike;
+  if (decoOver != null) meta.decoOver = decoOver;
+  if (decoUnder != null) meta.decoUnder = decoUnder;
+
+  if (decorationStyle != null) meta.decorationStyle = decorationStyle;
+  if (decorationStyleFromCssBorderStyle != null) {
+    switch (decorationStyleFromCssBorderStyle) {
+      case CssBorderStyle.dashed:
+        meta.decorationStyle = TextDecorationStyle.dashed;
+        break;
+      case CssBorderStyle.dotted:
+        meta.decorationStyle = TextDecorationStyle.dotted;
+        break;
+      case CssBorderStyle.double:
+        meta.decorationStyle = TextDecorationStyle.double;
+        break;
+      case CssBorderStyle.solid:
+        meta.decorationStyle = TextDecorationStyle.solid;
+        break;
+    }
+  }
+
+  if (fontFamily != null) meta.fontFamily = fontFamily;
+  if (fontSize != null) meta.fontSize = fontSize;
+  if (fontStyleItalic != null) meta.fontStyleItalic = fontStyleItalic;
+  if (fontWeight != null) meta.fontWeight = fontWeight;
+
+  if (isBlockElement != null) meta._isBlockElement = isBlockElement;
+  if (isNotRenderable != null) meta.isNotRenderable = isNotRenderable;
+
+  if (parentOps != null) {
+    assert(meta._parentOps == null);
+    meta._parentOps = parentOps;
+  }
+
+  if (stylesPrepend != null) {
+    styles = stylesPrepend;
+  }
+  if (styles != null) {
+    assert(styles.length % 2 == 0);
+    assert(!meta._stylesFrozen);
+    meta._styles ??= [];
+    if (styles == stylesPrepend) {
+      meta._styles.insertAll(0, styles);
+    } else {
+      meta._styles.addAll(styles);
+    }
+  }
+
+  return meta;
+}
+"""
+
+build_op_class="""
+class BuildOp {
+  final bool isBlockElement;
+
+  // op with lower priority will run first
+  final int priority;
+
+  final BuildOpDefaultStyles _defaultStyles;
+  final BuildOpOnChild _onChild;
+  final BuildOpOnPieces _onPieces;
+  final BuildOpOnWidgets _onWidgets;
+
+  BuildOp({
+    BuildOpDefaultStyles defaultStyles,
+    bool isBlockElement,
+    BuildOpOnChild onChild,
+    BuildOpOnPieces onPieces,
+    BuildOpOnWidgets onWidgets,
+    this.priority = 10,
+  })  : _defaultStyles = defaultStyles,
+        this.isBlockElement = isBlockElement ?? onWidgets != null,
+        _onChild = onChild,
+        _onPieces = onPieces,
+        _onWidgets = onWidgets;
+
+  bool get hasOnChild => _onChild != null;
+
+  List<String> defaultStyles(NodeMetadata meta, dom.Element e) =>
+      _defaultStyles != null ? _defaultStyles(meta, e) : null;
+
+  NodeMetadata onChild(NodeMetadata meta, dom.Element e) =>
+      _onChild != null ? _onChild(meta, e) : meta;
+
+  Iterable<BuiltPiece> onPieces(
+    NodeMetadata meta,
+    Iterable<BuiltPiece> pieces,
+  ) =>
+      _onPieces != null ? _onPieces(meta, pieces) : pieces;
+
+  Iterable<Widget> onWidgets(NodeMetadata meta, Iterable<Widget> widgets) =>
+      (_onWidgets != null ? _onWidgets(meta, widgets) : null) ?? widgets;
+}
+"""
+
+class TestParameterElements(unittest.TestCase):
+	"""TestParameterElements"""
+	def test_parameter_item(self):
+		pos = lazy_set_func.find("TextDecorationStyle decorationStyle")
+		parser = ParameterItemParser()
+		elem = parser.parse(lazy_set_func, pos-2)
+		self.assertEqual(elem.content(), "TextDecorationStyle decorationStyle")
+		self.assertEqual(elem.param_type.content(), "TextDecorationStyle")
+		self.assertEqual(elem.param_name.content(), "decorationStyle")
+
+if __name__ == '__main__':
+	unittest.main()
