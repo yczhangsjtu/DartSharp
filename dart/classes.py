e640166bf4487a869775d56802ea959dca827c43
Yuncong <yczhangsjtu@gmail.com>
2020-02-18 11:16:19 +0800

    Complete class header parser.

--- dart/classes.py
+++ dart/classes.py
@@ -1,7 +1,7 @@
-from eregex.element import BasicElement
+from eregex.element import BasicElement, JoinElement, ListElement
 from eregex.parser import OptionalParser, JoinParser,\
 	SpacePlainParser, SpaceParser, TypeNameParser,\
-	ListParser, OrParser
+	ListParser, OrParser, EmptyParser
 
 class ClassHeaderElement(BasicElement):
 	"""ClassHeaderElement"""
@@ -55,6 +55,30 @@ class ClassHeaderParser(object):
 			OptionalParser(ListParser(
 				ClassExtensionParser(),
 				EmptyParser()
-			))
+			)),
+			SpacePlainParser("{")
 		])
+
+	def parse(self, text, pos):
+		elem = self.parser.parse(text, pos)
+		if elem is None:
+			return None
+		is_abstract = type(elem[0]) is JoinElement
+		if is_abstract:
+			start = elem.start
+		else:
+			start = elem[1].start
+		name = elem[3]
+		extends, with_mixes, implements = None, None, None
+		if type(elem[4]) is ListElement:
+			for i in range(len(elem[4])):
+				extension_elem = elem[4][i]
+				if extension_elem.name.content() == "extends":
+					extends = extension_elem.elements
+				elif extension_elem.name.content() == "with":
+					with_mixes = extension_elem.elements
+				elif extension_elem.name.content() == "implements":
+					implements = extension_elem.elements
+		return ClassHeaderElement(text, start, elem[4].end, elem.span,\
+			name, is_abstract, extends, with_mixes, implements)
 
--- dart/test/classes_test.py
+++ dart/test/classes_test.py
@@ -1,5 +1,5 @@
 import unittest
-from dart.classes import ClassExtensionParser
+from dart.classes import ClassExtensionParser, ClassHeaderParser
 from dart.test.data import classes
 
 class TestClassHeader(unittest.TestCase):
@@ -34,6 +34,59 @@ class TestClassHeader(unittest.TestCase):
 		elem = parser.parse(classes, pos+36)
 		self.assertEqual(elem.content(), "with BaseClass")
 
+	def test_class_header(self):
+
+		parser = ClassHeaderParser()
+
+		pos = classes.find("abstract class MyClass")
+		elem = parser.parse(classes, pos-1)
+		self.assertEqual(elem.content(), "abstract class MyClass extends BaseClass")
+		self.assertEqual(elem.textspan(), "\nabstract class MyClass extends BaseClass {")
+		self.assertTrue(elem.is_abstract)
+		self.assertEqual(elem.name.content(), "MyClass")
+		self.assertEqual(elem.extends.content(), "BaseClass")
+		self.assertEqual(elem.with_mixes, None)
+		self.assertEqual(elem.implements, None)
+
+		pos = classes.find("class MySecondClass<T>")
+		elem = parser.parse(classes, pos-1)
+		self.assertEqual(elem.content(), "class MySecondClass<T> extends BaseClass<T>")
+		self.assertEqual(elem.textspan(), "\nclass MySecondClass<T> extends BaseClass<T> {")
+		self.assertFalse(elem.is_abstract)
+		self.assertEqual(elem.name.content(), "MySecondClass<T>")
+		self.assertEqual(elem.extends.content(), "BaseClass<T>")
+		self.assertEqual(elem.with_mixes, None)
+		self.assertEqual(elem.implements, None)
+
+		pos = classes.find("class MyThirdClass<T,K>")
+		elem = parser.parse(classes, pos-1)
+		self.assertEqual(elem.content(), """class MyThirdClass<T,K> implements MyClass, MySecondClass<T>
+                        with BaseClass""")
+		self.assertEqual(elem.textspan(), """\nclass MyThirdClass<T,K> implements MyClass, MySecondClass<T>
+                        with BaseClass {""")
+		self.assertFalse(elem.is_abstract)
+		self.assertEqual(elem.name.content(), "MyThirdClass<T,K>")
+		self.assertEqual(elem.name.name.content(), "MyThirdClass")
+		self.assertEqual(elem.with_mixes.content(), "BaseClass")
+		self.assertEqual(elem.extends, None)
+		self.assertEqual(elem.implements.content(), "MyClass, MySecondClass<T>")
+
+		pos = classes.find("abstract class MyFourthClass<T,K>")
+		elem = parser.parse(classes, pos-1)
+		self.assertEqual(elem.content(), """abstract class MyFourthClass<T,K>
+  extends MyThirdClass<T,K>
+  implements MyClass, MySecondClass<T>
+  with BaseClass""")
+		self.assertEqual(elem.textspan(), """\nabstract class MyFourthClass<T,K>
+  extends MyThirdClass<T,K>
+  implements MyClass, MySecondClass<T>
+  with BaseClass {""")
+		self.assertTrue(elem.is_abstract)
+		self.assertEqual(elem.name.content(), "MyFourthClass<T,K>")
+		self.assertEqual(elem.name.name.content(), "MyFourthClass")
+		self.assertEqual(elem.with_mixes.content(), "BaseClass")
+		self.assertEqual(elem.extends.content(), "MyThirdClass<T,K>")
+		self.assertEqual(elem.implements.content(), "MyClass, MySecondClass<T>")
 
 if __name__ == '__main__':
 	unittest.main()
